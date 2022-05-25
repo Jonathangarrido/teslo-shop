9b469bb4b500c3dc56f1c302d1a4eea077e256b1
jonathan <jonathan.garrido1988@gmail.com>
2022-05-25 19:20:41 -0400

    feat(bdd): lleno bdd y conexion a mongo

--- interfaces/products.ts
+++ interfaces/products.ts
@@ -10,6 +10,8 @@ export interface IProduct {
   title: string;
   type: ITypes;
   gender: 'men'|'women'|'kid'|'unisex'
+  createdAt: string
+  updatedAt: string
 }
 
 export type ISizes = 'XS'|'S'|'M'|'L'|'XL'|'XXL'|'XXXL';
