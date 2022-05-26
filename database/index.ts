b9f7607bb8970b1953c1b6ee3c1f256c882c7d7e
jonathan <jonathan.garrido1988@gmail.com>
2022-05-26 18:29:53 -0400

    feat(api): filtros consulta

--- database/index.ts
+++ database/index.ts
@@ -1,5 +1,5 @@
 export * as db from './db'
-
+export * from './constants'
 
 // Base de datos de prueba
 export * as seedDatabase from './products'
--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { db } from '../../../database'
+import { db, SHOP_CONSTANTS } from '../../../database'
 import { Product } from '../../../models'
 import { IProduct } from '../../../interfaces/products';
 
@@ -22,10 +22,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 }
 
 const getProducts = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
-
+  const { gender = 'all' } = req.query
+  let condition = {}
+  
+  if(gender !== 'all' && SHOP_CONSTANTS.validGenders.includes(`${gender}`)){
+    condition = { gender } 
+  }
+  
   await db.connect()
 
-  const products = await Product.find()
+  const products = await Product.find(condition)
                                 .select('title images price inStock slug -_id')
                                 .lean()
 
