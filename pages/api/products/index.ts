d32f0ed56c4941de006ff741bd00498179b553e4
jonathan <jonathan.garrido1988@gmail.com>
2022-05-26 18:38:14 -0400

    feat(api): obtener producto

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { db, SHOP_CONSTANTS } from '../../../database'
 import { Product } from '../../../models'
-import { IProduct } from '../../../interfaces/products';
+import { IProduct } from '../../../interfaces/'
 
 type Data = 
 | { message: string }
